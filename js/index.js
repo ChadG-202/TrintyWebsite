75953edbc5b9147abe218726706d196ee2720a13
ChadG-202 <chadgarratt202@gmail.com>
2022-09-05 16:04:09 +0100

    header changes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if($(window).scrollTop() >= $(window).height()){
+        if($(window).scrollTop() >= $(window).height()/2){
             $(".nav-wrapper").css({"background-color":"rgb(248, 248, 248)"});   
+            $(".nav-links").css({"padding-right":"0"}); 
+            $(".nav-logo").css({"margin-left":"20px"});   
         }
         else{
             $(".nav-wrapper").css({"background-color":"transparent"});
+            $(".nav-links").css({"padding-right":"28vw"}); 
+            $(".nav-logo").css({"margin-left":"-170px"});   
         }
 
     })
